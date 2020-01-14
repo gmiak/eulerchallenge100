534f56559f6cb61255a1b1e370b298bb567fa01d
Georges Kayembe <kayss.g@gmail.com>
2020-01-14 23:20:28 +0100

    add problem1&2

--- Problem1.java
+++ Problem1.java
@@ -1,2 +1,16 @@
 public class Problem1 {
+    public static void main(String[] args) {
+        System.out.println("The sum of all the multiples of 3 or 5 below 1000 is: "+sum(1000));
+    }
+
+    // Find the sum of all the multiples of 3 or 5 below n
+    public static int sum(int a) {
+        int theSum=0;
+        for (int i=3; i<a; i++){
+            if((i%3==0) || (i%5==0)) {
+                theSum = theSum+i;
+            }
+        }
+        return theSum;
+    }
 }
--- Problem2.java
+++ Problem2.java
@@ -1,2 +1,18 @@
 public class Problem2 {
+    public static void main(String[] args) {
+        System.out.println("The sum of the even-valued of n(th)term (n=43) = "+fiboEvenSum(43)+".");
+    }
+    // Find the sum of the even-valued terms in Fibonacci sequence and do not exceed the n(th) term.
+    public static int fiboEvenSum(int n){
+        int evenSum=0;
+        int n1=1, n2=2, n3=0;
+        for (int i=2; i<n; i++){
+            n3=n1+n2;
+            n1=n2;
+            if (n1%2==0)
+                evenSum+=n1;
+            n2=n3;
+        }
+        return evenSum;
+    }
 }
